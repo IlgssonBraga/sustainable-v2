6af555f8279e1fece179c49e8dabbcca78a1e275
Ilgsson Braga <ilgssonbraga@gmail.com>
2021-06-16 22:04:21 -0300

    feat: cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const {routes} = require('./routes/index.routes')
+const cors = require("cors");
 require("./database");
 
 const app = express()
 
 app.use(express.json());
+app.use(cors("http://localhost:8080"));
 
 const PORT = 3333
 
